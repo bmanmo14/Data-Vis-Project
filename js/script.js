1a02ef6746ac8ce7e6597348f8defff9e48a9544
Kyle Price <gc.price4@gmail.com>
2020-11-15 12:50:53 -0700

    Use single promise rather than two load calls

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
-async function load_data() {
-  return d3.csv('data/esg/CountryData.csv');
-}
-async function load_data2() {
-  return d3.csv('data/religion/relig.csv');
-}
-
-load_data().then(d => {
-  load_data2().then(d2 => {
-    const country_data = new CountryData(d, d2);
+Promise.all([d3.csv('data/esg/CountryData.csv'), d3.csv('data/religion/relig.csv')])
+  .then(([countries, religions]) => {
+    const country_data = new CountryData(countries, religions);
     const line_graph = new LineGraph(country_data);
-  });
 });
 
