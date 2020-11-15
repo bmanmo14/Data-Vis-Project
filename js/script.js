e3948080a0157aebe4f18c1ce790619f52d30b0b
Kyle Price <gc.price4@gmail.com>
2020-11-15 13:39:03 -0700

    Split relig and country data into class files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 Promise.all([d3.csv('data/esg/CountryData.csv'), d3.csv('data/religion/relig.csv')])
   .then(([countries, religions]) => {
-    const country_data = new CountryData(countries, religions);
+    const country_data = new DataOrchestrator(countries, religions);
     const line_graph = new LineGraph(country_data);
 });
 
